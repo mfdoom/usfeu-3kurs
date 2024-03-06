6011deec23b7a5d6ded9ce238cd5091444f1ae1a
mfdoom <ras.g57@gmail.com>
2024-03-06 20:23:24 +0500

    222

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const prod = {
-  SOCKET_URL: "http://metavoid.ru:90",
-  CALLBACK_URL: "http://metavoid.ru:91/",
+  SOCKET_URL: "https://metavoid.ru:90",
+  CALLBACK_URL: "https://metavoid.ru:91/",
 }
 const dev = {
   SOCKET_URL: "http://localhost:3001",
