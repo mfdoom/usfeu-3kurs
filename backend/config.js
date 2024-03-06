5fbd24e928df83149fb7117abbbfe0c1ed77a58c
mfdoom <ras.g57@gmail.com>
2024-03-06 23:07:22 +0500

    meta

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config()
 const prod = {
-  CLIENT_URL: "http://meta-void.ru/webdev",
-  CALLBACK_URL: "http://meta-void.ru:91/",
+  CLIENT_URL: "https://meta-void.ru/webdev",
+  CALLBACK_URL: "https://meta-void.ru:91/",
 }
 const dev = {
-  CLIENT_URL: "http://localhost:3000/webdev",
-  CALLBACK_URL: "http://localhost:91/",
+  CLIENT_URL: "https://localhost:3000/webdev",
+  CALLBACK_URL: "https://localhost:91/",
 }
 
 const config = process.env.NODE_ENV === "dev" ? dev : prod
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,8 +3,8 @@ const prod = {
   CALLBACK_URL: "https://meta-void.ru:91/",
 }
 const dev = {
-  SOCKET_URL: "http://localhost:3001",
-  CALLBACK_URL: "http://localhost:91/",
+  SOCKET_URL: "https://localhost:3001",
+  CALLBACK_URL: "https://localhost:91/",
 }
 
 export const config = process.env.REACT_APP_STATUS === "dev" ? dev : prod
