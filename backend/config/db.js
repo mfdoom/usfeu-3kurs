ddb18a95db4c2c2144b155335b6c541e2ba30a2c
mfdoom <ras.g57@gmail.com>
2024-04-05 22:19:45 +0500

    env folder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-dotenv.config()
+dotenv.config({ path: "../.env" })
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
